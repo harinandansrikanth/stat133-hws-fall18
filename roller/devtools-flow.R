efebc7cd605c8ced355ab1655fc98bf0ab46ff67
harinandansrikanth <42632521+harinandansrikanth@users.noreply.github.com>
2018-12-02 22:36:14 -0800

    Delete devtools-flow.R

--- roller/devtools-flow.R
+++
@@ -1,9 +0,0 @@
-devtools::document() #object documentation
-devtools::check_man() #runs most common checks of object documentation
-devtools::test() #tests package
-devtools::build_vignettes() #builds all vignettes from the console
-devtools::build() #creates a package bundle (includes vignettes)
-devtools::install() #installs package
-#devtools::install_github("harinandansrikanth/stat133-hws-fall18/roller", build_vignettes = TRUE)
-#devtools::install_github("harinandansrikanth/stat133-hws-fall18/roller")
-#devtools::check() #runs all of the checks of object documentation
